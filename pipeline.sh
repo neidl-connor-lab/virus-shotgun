19b0f863ee93a259a0923935154d24b4b783f265
Jacquelyn Turcinovic <jturcino@bu.edu>
2024-02-27 09:35:15 -0500

    updated pipeline.sh (untested)

--- pipeline.sh
+++ pipeline.sh
@@ -2,11 +2,11 @@
 
 # qsub options
 #$ -l h_rt=24:00:00
-#$ -pe omp 8
+#$ -pe omp 4
 #$ -j y
 #$ -o log-$JOB_NAME.qlog
 
-## setup --------------------------------------------------------
+## setup -----------------------------------------------------------------------
 # functions
 mesg () { echo -e "[MSG] $@"; }
 err () { echo -e "[ERR] $@"; exit 1; }
@@ -22,21 +22,25 @@ checkcmd () {
 # pre-set variables
 LOFREQ="pipeline/lofreq/lofreq"
 KDB="pipeline/kraken2db"
+FORMAT="pipeline/format.r"
+MINCOV="10"
 # help message
-HELP="usage: qsub -P PROJECT -N JOBNAME $0 -i INDEX -f FASTA -o ODIR -x R1 [-y R2]
+HELP="usage: qsub -P PROJECT -N JOBNAME $0 -i INDEX -f FASTA -o ODIR -s SAMPLE -x R1 [-y R2] [-t THLD]
 Please submit the job from the pipeline directory!
 
 arguments:
   -i bowtie2 index path and prefix
   -f reference FASTA
-  -o output directory (e.g., sample ID)
+  -o output directory
+  -s sample ID
   -x FASTQ file; R1 file if paired reads
   -y [OPTIONAL] R2 FASTQ file if paired reads
+  -t [OPTIONAL] minimum aligned read depth (default: $MINCOV)
   -h print this message and exit
 "
 
 # parsing arguments
-while getopts ":hi:f:o:s:x:y:" opt 
+while getopts ":hi:f:o:s:x:y:t:" opt 
 do 
   case ${opt} in 
     i ) IDX="${OPTARG}"
@@ -45,9 +49,13 @@ do
       ;;
     o ) ODIR="${OPTARG}"
       ;;
+    s ) SAMPLE="${OPTARG}"
+      ;;
     x ) R1="${OPTARG}"
       ;;
     y ) R2="${OPTARG}"
+      ;;
+    t ) MINCOV="${OPTARG}"
       ;;
     h ) echo "$HELP" && exit 0
       ;;
@@ -57,7 +65,7 @@ do
 done
 shift $((OPTIND -1))
 
-## print job info for output log --------------------------------
+## print job info for output log -----------------------------------------------
 echo "=========================================================="
 echo "Start date: $(date)"
 echo "Running on node: $(hostname)"
@@ -67,7 +75,7 @@ echo "Job ID : $JOB_ID"
 echo "=========================================================="
 echo ""
 
-## check inputs -------------------------------------------------
+## check inputs ----------------------------------------------------------------
 mesg "STEP 0: CHECKING INPUTS"
 
 # if no kraken, fail
@@ -76,25 +84,30 @@ then
   err "No Kraken2 found. Please install and try again."
 fi
 
-# if no bowtie2, load module
-if [ -z "$(bowtie2 --version 2> /dev/null)" ]
-then
-  module load bowtie2
-  checkcmd "Loading bowtie2"
-fi
-
-# if no samtools, load module
-if [ -z "$(samtools version 2> /dev/null)" ]
-then
-  module load samtools
-  checkcmd "Loading samtools"
-fi
-
 # double-check that lofreq exists
 if [ -z "$($LOFREQ version 2> /dev/null)" ]
 then
   err "LoFreq error: $LOFREQ"
 fi
+
+# double-check the formatting script exists
+if [ ! -f "$FORMAT" ]
+then 
+  err "VCF formatting script not detected: $FORMAT"
+fi
+
+# load bowtie2 module
+module load bowtie2/2.4.2
+checkcmd "Loading bowtie2"
+
+# load samtools module 
+module load htslib/1.18
+module load samtools/1.18
+checkcmd "Loading samtools"
+
+# load R
+module load R/4.0.2
+checkcmd "Loading R"
 
 # bowtie2 index should have 6 files
 if [ -z "$IDX" ]
@@ -107,8 +120,8 @@ else
   err "Invalid bowtie2 index; run setup.sh first: $IDX"
 fi
 
-# kraken2 db shoudl have 4 files
-if [ "$(ls -1 $KDB 2> /dev/null | wc -l)" -ne 11 ]
+# kraken2 db should have 12 files
+if [ "$(ls -1 $KDB 2> /dev/null | wc -l)" -ne 12 ]
 then
   err "Invalid Kraken2 database; rerun setup.sh"
 fi
@@ -136,6 +149,19 @@ else
   mkdir -p "$ODIR"
 fi
 
+# check sample-specific output directory
+VAR="$ODIR/$SAMPLE"
+if [ -z "$SAMPLE" ]
+then
+  err "No sample ID provided"
+elif [ ! -d "$VAR" ]
+then
+  mesg "Outputting files to: $VAR"
+  mkdir -p "$VAR"
+else
+  mesg "Outputting files to: $VAR"
+fi
+
 # R1/R0 FASTQ file
 if [ -z "$R1" ]
 then
@@ -158,15 +184,23 @@ else
   err "Invalid second FASTQ file: $R2"
 fi
 
+# minimum coverage
+if [ -z "$MINCOV" ]
+then
+  err "No minimum coverage specified"
+else
+  mesg "Minimum aligned read depth: $MINCOV"
+fi
+
 # done checking inputs!
 mesg "Done checking inputs!"
 echo ""
 
-## contaminant check --------------------------------------------
+## contaminant check -----------------------------------------------------------
 mesg "STEP 1: CONTAMINANT CHECK"
 
 # build command based on whether has paired reads
-CMD="kraken2 --db '$KDB' --threads 8 --output - --report '$ODIR/metagenomics.tsv' --use-names --gzip-compressed"
+CMD="kraken2 --db '$KDB' --threads 4 --output - --report '$VAR/metagenomics.tsv' --use-names"
 if [ -z "$R2" ] # unpaired
 then
   CMD="$CMD '$R1'"
@@ -181,14 +215,14 @@ checkcmd "Kraken2 metagenomic classification"
 echo ""
 
 ## alignment ----------------------------------------------------
-mesg "STEP 2: ALIGN TO GENOME"
+mesg "STEP 2: ALIGN TO VIRAL GENOME"
 
 # build command based on whether has paired reads
 if [ -z "$R2" ] # unpaired
 then
-  CMD="bowtie2 --threads 8 -x '$IDX' -U '$R1' > '$ODIR/alignment.sam'"
+  CMD="bowtie2 --threads 4 -x '$IDX' -U '$R1' 1> '$VAR/alignment.sam' 2> '$VAR/bowtie2.log'"
 else # paired
-  CMD="bowtie2 --threads 8 -x '$IDX' -1 '$R1' -2 '$R2' > '$ODIR/alignment.sam'"
+  CMD="bowtie2 --threads 4 -x '$IDX' -1 '$R1' -2 '$R2' 1> '$VAR/alignment.sam' 2> '$VAR/bowtie2.log'"
 fi
 
 # run alignment
@@ -197,32 +231,32 @@ eval "$CMD"
 checkcmd "Alignment"
 
 # compress SAM to BAM
-CMD="samtools view --threads 8 -b -h '$ODIR/alignment.sam' > '$ODIR/alignment-raw.bam'"
+CMD="samtools view --threads 4 -b -h '$VAR/alignment.sam' > '$VAR/alignment-raw.bam'"
 mesg "CMD: $CMD"
 eval "$CMD"
 checkcmd "Compression"
-rm "$ODIR/alignment.sam"
+rm "$VAR/alignment.sam"
 echo ""
 
 ## process BAM --------------------------------------------------
 mesg "STEP 3: PROCESS BAM"
 
 # sort BAM
-CMD="samtools sort --threads 8 '$ODIR/alignment-raw.bam' > '$ODIR/alignment-sorted.bam'"
+CMD="samtools sort --threads 4 '$VAR/alignment-raw.bam' > '$VAR/alignment-sorted.bam'"
 mesg "CMD: $CMD"
 eval "$CMD"
 checkcmd "Sorting"
-rm "$ODIR/alignment-raw.bam"
+rm "$VAR/alignment-raw.bam"
 
 # score indels to get final BAM
-CMD="$LOFREQ indelqual --dindel --ref '$REFSEQ' '$ODIR/alignment-sorted.bam' > '$ODIR/alignment.bam'"
+CMD="$LOFREQ indelqual --dindel --ref '$REFSEQ' '$VAR/alignment-sorted.bam' > '$VAR/alignment.bam'"
 mesg "CMD: $CMD"
 eval "$CMD"
 checkcmd "Indelqual"
-rm "$ODIR/alignment-sorted.bam"
+rm "$VAR/alignment-sorted.bam"
 
 # index final BAM
-CMD="samtools index '$ODIR/alignment.bam'"
+CMD="samtools index '$VAR/alignment.bam'"
 mesg "CMD: $CMD"
 eval "$CMD"
 checkcmd "Indexing"
@@ -232,7 +266,7 @@ echo ""
 mesg "STEP 4: CALCULATE COVERAGE"
 
 # coverage with samtools depth
-CMD="samtools depth --threads 8 -a -H '$ODIR/alignment.bam' > '$ODIR/coverage.tsv'"
+CMD="samtools depth --threads 4 -a -H '$VAR/alignment.bam' > '$VAR/coverage.tsv'"
 mesg "CMD: $CMD"
 eval "$CMD"
 checkcmd "Coverage"
@@ -242,16 +276,16 @@ echo ""
 mesg "STEP 5: ASSEMBLE CONSENSUS"
 
 # consensus with samtools
-CMD="samtools consensus --threads 8 --use-qual --min-depth 10 --call-fract 0.5 --output '$ODIR/consensus-tmp.fa' '$ODIR/alignment.bam'"
+CMD="samtools consensus --threads 4 --use-qual --min-depth $MINCOV --call-fract 0.5 --output '$VAR/consensus-tmp.fa' '$VAR/alignment.bam'"
 mesg "CMD: $CMD"
 eval "$CMD"
 checkcmd "Consensus"
 
 # update consensus header
 mesg "Updating consensus header"
-echo ">$ID" > "$ODIR/consensus.fa"
-cat "$ODIR/consensus-tmp.fa" | grep "^[^>]" >> "$ODIR/consensus.fa"
-rm "$ODIR/consensus-tmp.fa"
+echo ">$SAMPLE" > "$VAR/consensus.fa"
+cat "$VAR/consensus-tmp.fa" | grep "^[^>]" >> "$VAR/consensus.fa"
+rm "$VAR/consensus-tmp.fa"
 echo ""
 
 ## quantify SNVs ------------------------------------------------
@@ -259,10 +293,17 @@ mesg "STEP 6: QUANTIFY SNVs"
 
 # run lofreq
 # keeping mapping quality parameters same between samtools and LoFreq
-CMD="$LOFREQ call-parallel --pp-threads 8 --call-indels --min-cov 10 --ref '$REFSEQ' '$ODIR/alignment.bam' > '$ODIR/snvs.vcf'"
+CMD="$LOFREQ call-parallel --pp-threads 4 --call-indels --min-cov $MINCOV --ref '$REFSEQ' '$VAR/alignment.bam' > '$VAR/snvs.vcf'"
 mesg "CMD: $CMD"
 eval "$CMD"
 checkcmd "LoFreq"
+echo ""
+
+# format VCF
+CMD="Rscript $FORMAT --vcf '$VAR/snvs.vcf' --ofile '$VAR/snvs.csv'"
+mesg "CMD: $CMD"
+eval "$CMD"
+checkcmd "SNV formatting"
 echo ""
 
 ## package version ----------------------------------------------
@@ -273,5 +314,3 @@ echo ""
 echo "LoFreq"
 $LOFREQ version
 echo ""
-samtools --version
-echo "" 
